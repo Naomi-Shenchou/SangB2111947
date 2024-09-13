48f0617a94e308b4e6f56e56dfc3df3ee515ab8c
Naomi <trithanhsang203@gmail.com>
2024-09-13 08:23:17 +0700

    Standardize JSON responses

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,39 +1,40 @@
+const JSend = require('../jsend');
 function createContact(req, res) {
-    return res.status(201).json({ contact: {} });
+return res.status(201).json(JSend.success({ contact: {} }));
 }
 function getContactsByFilter(req, res) {
-    const filters = [];
-    const { favorite, name } = req.query;
-    if (favorite !== undefined) {
-        filters.push(`favorite=${favorite}`);
-    }
-    if (name) {
-        filters.push(`name=${name}`);
+const filters = [];
+const { favorite, name } = req.query;
+if (favorite !== undefined) {
+filters.push(`favorite=${favorite}`);
+}
+if (name) {
+    filters.push(`name=${name}`);
     }
     console.log(filters.join('&'));
-    return res.json({ contacts: [] });
-}
-function getContact(req, res) {
-    return res.json({ contact: {} });
-}
-function updateContact(req, res) {
-    return res.json({ contact: {} });
-}
-function deleteContact(req, res) {
-    return res.json({
-    message: 'Contact deleted',
-});
-}
-function deleteAllContacts(req, res) {
-    return res.json({
-    message: 'All contacts deleted',
-});
-}
-module.exports = {
+    return res.json(
+    JSend.success({
+    contacts: [],
+    })
+    );
+    }
+    function getContact(req, res) {
+    return res.json(JSend.success({ contact: {} }));
+    }
+    function updateContact(req, res) {
+    return res.json(JSend.success({ contact: {} }));
+    }
+    function deleteContact(req, res) {
+    return res.json(JSend.success());
+    }
+    function deleteAllContacts(req, res) {
+    return res.json(JSend.success());
+    }
+    module.exports = {
     getContactsByFilter,
     deleteAllContacts,
     getContact,
     createContact,
     updateContact,
     deleteContact,
- };   
+    };
