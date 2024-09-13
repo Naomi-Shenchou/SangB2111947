c12224814e7ea3c1426deecdf2c510e0496d5b27
Naomi <trithanhsang203@gmail.com>
2024-09-13 08:53:14 +0700

    Implement error handlers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
+
+const {
+    resourceNotFound,
+    handleError,
+} = require('./controllers/errors.controller');
+
 const app = express();
 
 app.use(cors());
@@ -11,4 +17,10 @@ app.get('/', (req, res) => {
     return res.json(JSend.success());
 });
 contactsRouter.setup(app);
+
+// Handle 404 response
+app.use(resourceNotFound);
+
+//Define error-handling middleware last, after other app.use() and routes calls
+app.use(handleError);
 module.exports = app;
